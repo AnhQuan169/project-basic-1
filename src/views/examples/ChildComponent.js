13103d7502b8a6642b48a463e7194779515b5e47
AnhQuan169 <anhquannguyen169@gmail.com>
2022-02-16 16:53:01 +0700

    Stateless-Statefull Components

--- src/views/examples/ChildComponent.js
+++ src/views/examples/ChildComponent.js
@@ -1,36 +1,60 @@
 import React from 'react';
 
-class ChildComponent extends React.Component {
+// class ChildComponent extends React.Component {
 
     
-    render() {
-        console.log('Props:',this.props)
-        // let name = this.props.name;
-        // let age = this.props.age;
+//     render() {
+//         console.log('Props:',this.props)
+//         // let name = this.props.name;
+//         // let age = this.props.age;
 
-        //Cấu trúc ngắn gọn hơn khi khai báo 1 biến của 1 props
-        //Bắt buộc key:value ( tên của biến khai báo phải hoàn toàn giống value của props)
-        let {name,age, arrJobs} = this.props;
+//         //Cấu trúc ngắn gọn hơn khi khai báo 1 biến của 1 props
+//         //Bắt buộc key:value ( tên của biến khai báo phải hoàn toàn giống value của props)
+//         let {name,age, arrJobs} = this.props;
         
-        return (
-            <>
-                <div className="job-lists">
-                {
-                    //Vòng lặp map
-                    //item: từng phần tử bên trong array
-                    //index: chỉ số của mảng này
-                    arrJobs.map((item, index) => {
-                        return (
-                            <div key={item.id}>
-                                {item.title} - Salary: {item.salary}
-                            </div>
-                        )
-                    })
-                }
-                </div>
-            </>
-        )
-    }
+//         return (
+//             <>
+//                 <div className="job-lists">
+//                 {
+//                     //Vòng lặp map
+//                     //item: từng phần tử bên trong array
+//                     //index: chỉ số của mảng này
+//                     arrJobs.map((item, index) => {
+//                         return (
+//                             <div key={item.id}>
+//                                 {item.title} - Salary: {item.salary}
+//                             </div>
+//                         )
+//                     })
+//                 }
+//                 </div>
+//             </>
+//         )
+//     }
+// }
+
+//Định nghĩa 1 Function
+const ChildComponent = (props) =>{
+    let {name,age, arrJobs} = props;
+        
+    return (
+        <>
+            <div className="job-lists">
+            {
+                //Vòng lặp map
+                //item: từng phần tử bên trong array
+                //index: chỉ số của mảng này
+                arrJobs.map((item, index) => {
+                    return (
+                        <div key={item.id}>
+                            {item.title} - Salary: {item.salary}
+                        </div>
+                    )
+                })
+            }
+            </div>
+        </>
+    )
 }
 
 export default ChildComponent;
